d8ba37b0dffeba88e363a5d04ff394d5baa6ee67
marmar <meriemiag@yahoo.fr>
2024-03-28 04:10:20 +0100

    update lambda code bucket

--- lambda.tf
+++ lambda.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "texttospeech" {
 
   s3_bucket         = data.aws_s3_bucket_object.lambda_zip.bucket
   s3_key            = data.aws_s3_bucket_object.lambda_zip.key
-  s3_object_version = data.aws_s3_bucket_object.lambda_zip.version_id
+  //s3_object_version = data.aws_s3_bucket_object.lambda_zip.version_id
 
   # The handler must be name_of_lambda_code_file.name_of_your_code_handler 
   handler = "lambda.lambda_handler"
